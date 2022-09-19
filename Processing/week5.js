b647e8d9a2a56901751bb53aaad4406bb83c5bb0
PietroDeZen <111724497+PietroDeZen@users.noreply.github.com>
2022-09-19 16:41:06 +1000

    Add files via upload

---
+++ Processing/week5.js
@@ -0,0 +1,12 @@
+
+   
+
+
+
+
+
+
+// the statement circleX +1 is evaluated // circleX = 100 // circleX = circleX + 1 creates a loop
+// the variable is constantly updated // the loop won't stop if no intervention takes place
+
+
